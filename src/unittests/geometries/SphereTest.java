a47d4b90e2ff7d44418d66af4dcc2e685aced27a
EsterNadler <ester08919@gmail.com>
2023-03-28 10:10:03 +0300

    tests

---
+++ src/unittests/geometries/SphereTest.java
@@ -0,0 +1,24 @@
+/**
+ * 
+ */
+package unittests.geometries;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+/**
+ * @author ester
+ *
+ */
+class SphereTest {
+
+	/**
+	 * Test method for {@link geometries.Sphere#getNormal(primitives.Point)}.
+	 */
+	@Test
+	void testGetNormal() {
+		fail("Not yet implemented");
+	}
+
+}
