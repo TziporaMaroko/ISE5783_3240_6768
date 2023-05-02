09f2f96948ce9a744d2932d5b326eaa57f6d347c
EsterNadler <ester08919@gmail.com>
2023-05-02 12:50:09 +0300

    middle

---
+++ src/unittests/CameraRayIntegration.java
@@ -0,0 +1,21 @@
+/**
+ * 
+ */
+package unittests;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+/**
+ * @author ester
+ *
+ */
+class CameraRayIntegration {
+
+	@Test
+	void test() {
+		fail("Not yet implemented");
+	}
+
+}
