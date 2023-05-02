0c9d2d734cdb4a78595e1ecd68aa11bedba8b3a0
EsterNadler <ester08919@gmail.com>
2023-05-02 12:02:46 +0300

    camera

--- src/renderer/Camera.java
+++ src/renderer/Camera.java
@@ -1,5 +1,150 @@
+/**
+ * 
+ */
 package renderer;
 
+import primitives.*;
+import static primitives.Util.*;
+
+/**
+ * @author tzipora and ester:)
+ * Camera class
+ */
 public class Camera {
+	private Point p0;
+	private Vector vUp;
+	private Vector vRight;
+	private Vector vTo;
+	private double width;
+	private double height;
+	private double distance;
+	
+	/**
+	 * ctor
+	 * @param p0 
+	 * @param vTo
+	 * @param vUp
+	 */
+	public Camera(Point p0, Vector vTo, Vector vUp) /*throws Exception*/{
+		if(!isZero(vTo.dotProduct(vUp))) 
+			throw new IllegalArgumentException("vUp is not ortogonal to vTo");
+
+		this.vTo = vTo.normalize();
+		this.vUp = vUp.normalize();
+		vRight = (vTo.crossProduct(vUp)).normalize();
+		
+		this.p0 = p0;
+	}
+	
+	/**
+	 * sets the sizes of view plane
+	 * @param width
+	 * @param height
+	 * @return the camera
+	 */
+	public Camera setVPSize(double width, double height){
+		this.width = width;
+		this.height = height;
+		return this;
+	}
+	
+	/**
+	 * sets the distance between the camera and the view plane
+	 * @param distance
+	 * @return the camera
+	 */
+	public Camera setVPDistance(double distance){
+		this.distance = distance;
+		return this;
+	}
+	
+	/**
+	 * 
+	 * @param nX
+	 * @param nY
+	 * @param j
+	 * @param i
+	 * @return
+	 */
+	public Ray constructRay(int nX, int nY, int j, int i)/*throws Exception*/{//constructRayThroughPixel
+return null;
+		//		Point Pc;
+//		if (isZero(distance))
+//			Pc=p0;
+//		else
+//			Pc=p0.add(vTo.scale(distance));
+//		
+//		double Ry= height/nY;
+//		double Rx=width/nX;
+//		double Yi=(i-(nY-1)/2d)*Ry;///////////////
+//		double Xj=(j-(nX-1)/2d)*Rx;
+//		
+//		if(isZero(Xj) && isZero(Yi))
+//			return new Ray (p0, Pc.subtract(p0));
+//		
+//		Point Pij = Pc;
+//		
+//		if(!isZero(Xj))
+//			Pij = Pij.add(vRight.scale(Xj));
+//		
+//		if(!isZero(Yi))
+//			Pij = Pij.add(vUp.scale(-Yi));
+//		
+//		Vector Vij = Pij.subtract(p0);
+//		
+//		if(Pij.equals(p0))
+//			return new Ray(p0, new Vector(Pij.getX(), Pij.getY(), Pij.getZ()));
+//		return new Ray(p0, Vij);
+	}
+
+	/**
+	 * @return the p0
+	 */
+	public Point getP0() {
+		return p0;
+	}
+
+	/**
+	 * @return the vUp
+	 */
+	public Vector getvUp() {
+		return vUp;
+	}
+
+	/**
+	 * @return the vRight
+	 */
+	public Vector getvRight() {
+		return vRight;
+	}
+
+	/**
+	 * @return the vTo
+	 */
+	public Vector getvTo() {
+		return vTo;
+	}
+
+	/**
+	 * @return the width
+	 */
+	public double getWidth() {
+		return width;
+	}
+
+	/**
+	 * @return the height
+	 */
+	public double getHeight() {
+		return height;
+	}
+
+	/**
+	 * @return the distance
+	 */
+	public double getDistance() {
+		return distance;
+	}
 
 }
+
