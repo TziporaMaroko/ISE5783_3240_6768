2e4ad7baba1af330abb2a3c1ad27b6a0c45a5dfe
Tzipora Maroko <maroko@g.jct.ac.il>
2023-05-09 11:11:21 +0300

    stage 5 start

---
+++ src/unittests/ImageWriterTest.java
@@ -0,0 +1,21 @@
+/**
+ * 
+ */
+package unittests;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+/**
+ * @author DELL
+ *
+ */
+class ImageWriterTest {
+
+	@Test
+	void test() {
+		fail("Not yet implemented");
+	}
+
+}
